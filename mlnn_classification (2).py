d369a84eda0fe34ab76c3b389c6f75491f684bff
vaniamzf <45489267+vaniamzf@users.noreply.github.com>
2020-02-28 11:18:03 +0700

    commit

---
+++ mlnn_classification (2).py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welcome to Jupyter!
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
+# 
+# Outline of some basics:
+# 
+# * [Notebook Basics](../examples/Notebook/Notebook%20Basics.ipynb)
+# * [IPython - beyond plain python](../examples/IPython%20Kernel/Beyond%20Plain%20Python.ipynb)
+# * [Markdown Cells](../examples/Notebook/Working%20With%20Markdown%20Cells.ipynb)
+# * [Rich Display System](../examples/IPython%20Kernel/Rich%20Output.ipynb)
+# * [Custom Display logic](../examples/IPython%20Kernel/Custom%20Display%20Logic.ipynb)
+# * [Running a Secure Public Notebook Server](../examples/Notebook/Running%20the%20Notebook%20Server.ipynb#Securing-the-notebook-server)
+# * [How Jupyter works](../examples/Notebook/Multiple%20Languages%2C%20Frontends.ipynb) to run code in different languages.
+
+# In[3]:
+
+
+import numpy as np
+
+
+# You can also get this tutorial and run it on your laptop:
+# 
+#     git clone https://github.com/ipython/ipython-in-depth
+# 
+# Install IPython and Jupyter:
+# 
+# with [conda](https://www.anaconda.com/download):
+# 
+#     conda install ipython jupyter
+# 
+# with pip:
+# 
+#     # first, always upgrade pip!
+#     pip install --upgrade pip
+#     pip install --upgrade ipython jupyter
+# 
+# Start the notebook in the tutorial directory:
+# 
+#     cd ipython-in-depth
+#     jupyter notebook
+
+# In[4]:
+
+
+import sklearn
+
+
+# In[5]:
+
+
+import sklearn.datasets
+
+
+# In[6]:
+
+
+import matplotlib
+
+
+# In[7]:
+
+
+#Display plots inline and changes default figure size
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
+
+
+# In[10]:
+
+
+#%% 2
+np.random.seed(3)
+X, y = sklearn.datasets.make_moons(200, noise=0.20)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+
+
+# In[ ]:
+
+
+
+
---
+++ mlnn_classification (4).py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welcome to Jupyter!
+
+# In[1]:
+
+
+num_examples = len(X) #training set size
+nn_input_dim = 2 #input layer dimensionality
+nn_output_dim = 2 #output layer dimensionality
+
+#Gradient descent parameters (I picked these by hand)
+epsilon = 0.01 #learning rate for gradient descent
+reg_lambda = 0.01 #regularisation strength
+
+
+# This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
+# 
+# Outline of some basics:
+# 
+# * [Notebook Basics](../examples/Notebook/Notebook%20Basics.ipynb)
+# * [IPython - beyond plain python](../examples/IPython%20Kernel/Beyond%20Plain%20Python.ipynb)
+# * [Markdown Cells](../examples/Notebook/Working%20With%20Markdown%20Cells.ipynb)
+# * [Rich Display System](../examples/IPython%20Kernel/Rich%20Output.ipynb)
+# * [Custom Display logic](../examples/IPython%20Kernel/Custom%20Display%20Logic.ipynb)
+# * [Running a Secure Public Notebook Server](../examples/Notebook/Running%20the%20Notebook%20Server.ipynb#Securing-the-notebook-server)
+# * [How Jupyter works](../examples/Notebook/Multiple%20Languages%2C%20Frontends.ipynb) to run code in different languages.
+
+# You can also get this tutorial and run it on your laptop:
+# 
+#     git clone https://github.com/ipython/ipython-in-depth
+# 
+# Install IPython and Jupyter:
+# 
+# with [conda](https://www.anaconda.com/download):
+# 
+#     conda install ipython jupyter
+# 
+# with pip:
+# 
+#     # first, always upgrade pip!
+#     pip install --upgrade pip
+#     pip install --upgrade ipython jupyter
+# 
+# Start the notebook in the tutorial directory:
+# 
+#     cd ipython-in-depth
+#     jupyter notebook
